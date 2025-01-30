b16ce49c52a201d915d5ece74551de6609eb3bb3
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2025-01-29 23:30:04 -0800

    Extended documentation slightly, included goals, started on file handling.

--- README.md
+++ README.md
@@ -4,12 +4,35 @@ A bot for Python-GSOC's Fediverse presence.
 
 Making it easier to keep track of our participants, and inviting both participants and mentors to engage more in the community. Plus, it's written in Python! We figure we should be able to hack on our own infrastructure, in the project language.
 
-## Features
+## Features (goals)
 
 - Keep track of participants' regular posts
 - Link participants, projects, and mentors
 - Produce reports
 - Promote projects?
+
+## Usage
+
+```bash
+usage: python-gsoc-bot [-h] -d DEADLINE (-i INPUT_FILE | -a ACCOUNT) [-v]
+
+Checks up on users.
+
+options:
+  -h, --help            show this help message and exit
+  -d DEADLINE, --deadline DEADLINE
+                        The date/time (prefer RFC 3339, indicating UTC with a +0.)
+  -i INPUT, --input INPUT_FILE
+                        Path to a file containing a list of accounts, not yet implemented.
+  -a ACCOUNT, --account ACCOUNT
+                        Account name for a single account to look up. For the local server, omit the server address.
+  -v, --verbose         Log while running
+```
+
+Notes:
+
+File input support isn't yet ready at all, and reporting's not at all there yet. Actual posting has
+yet to be designed.
 
 ## Contribute?
 
--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 # 
 
+import mastodon
 from mastodon import Mastodon
-from pathlib import PosixPath
-import os.path
+from pathlib import Path,PosixPath
+
 import argparse
 from datetime import datetime, timedelta, timezone
 import dateparser
 
+trace_level = None
+
 python_gsoc_url = 'https://social.python-gsoc.org/'
 client_id = 'psf-gsoc-bot'
 cred_path = PosixPath('~/.psf-gsoc-bot/clientcred.secret').expanduser()
+
+def trace(message: str):
+    if trace_level is not None:
+        print(message)
 
 def check_recent_posts(client: Mastodon, account_name: str, deadline: datetime):
     today = datetime.now(timezone.utc)
@@ -17,68 +24,97 @@ def check_recent_posts(client: Mastodon, account_name: str, deadline: datetime):
     week = timedelta(weeks=1)
     initial_window = deadline - week
 
-    # take an account name
-    account = client.account_lookup(account_name)
+    try:
+        # take an account name
+        account = client.account_lookup(account_name)
+        print(f"{account_name}:")
 
-    # look up the posts, in order. Find the most recent.
-    posts = client.account_statuses(account['id'], exclude_reblogs = True, exclude_replies = True, limit=10)
-    print(f'Returned {len(posts)} posts.')
+        # look up the posts, in order. Find the most recent.
+        posts = client.account_statuses(account['id'], exclude_reblogs = True, exclude_replies = True, limit=10)
+        trace(f'Returned {len(posts)} posts.')
 
-    for post in posts:
-        print(post)
+        for post in posts:
+            trace(post)
 
-    if (len(posts) > 0):
-        post = posts[0]
-        postdate = post['created_at']
-        print()
-        # Record when the most recent post was
-        print(f"Most recent post posted on: ({type(postdate)}) {postdate}, contents: {post['content']}")
+        if (len(posts) > 0):
+            post = posts[0]
+            postdate = post['created_at']
 
-        # check the current date against the most recent deadline date.
-        # if the most recent post is not within a week of the most recent deadline,
-        if (today > deadline and postdate < initial_window):
-            # log that they're behind.
-            print(f"{account_name} hasn't posted in over a week.")
-            # send them a message and flag them in a list to the mentors & admins
+            # Record when the most recent post was
+            print(f"Most recent post posted on: ({type(postdate)}) {postdate}, contents: {post['content']}")
 
-
+            # check the current date against the most recent deadline date.
+            # if the most recent post is not within a week of the most recent deadline,
+            if (today > deadline and postdate < initial_window):
+                # log that they're behind.
+                print(f"{account_name} hasn't posted in over a week.")
+                # send them a message and flag them in a list to the mentors & admins
+    except mastodon.errors.MastodonNotFoundError:
+        print(f"{account_name} is not present on the server.")
 
 def register_app():
-    print("Checking for registration.")
+    trace("Checking for registration.")
     if not cred_path.exists():
-        print('No registration found.')
+        trace('No registration found.')
         if not cred_path.parent.exists():
-            print(f'Config directory {cred_path.parent} missing.')
+            trace(f'Config directory {cred_path.parent} missing.')
             cred_path.parent.mkdir()
-        print(f'Registering as {client_id}.')
+        trace(f'Registering as {client_id}.')
         Mastodon.create_app(
             client_id,
             api_base_url = python_gsoc_url,
             to_file = cred_path
         )
 
-register_app()
-cred = cred_path.read_text()
-
-mastodon = Mastodon(
-    client_id,
-    cred,
-    api_base_url = python_gsoc_url
-)
-
-parser = argparse.ArgumentParser(prog='python-gsoc bot',
+parser = argparse.ArgumentParser(prog='python-gsoc-bot',
                                  description='Checks up on users.')
 
-parser.add_argument('-d', '--deadline')
+parser.add_argument('-d', '--deadline', required=True,
+                    help="The date/time (prefer RFC 3339, indicating UTC with a +0.)")
+input_group = parser.add_mutually_exclusive_group(required=True)
+input_group.add_argument('-i', '--input', metavar='INPUT_FILE',
+                         help="Path to a file containing a list of accounts, see notes.")
+input_group.add_argument('-a', '--account',
+                         help="Account name for a single account to look up. For the local server, omit the server address.")
+parser.add_argument('-v', '--verbose', action='store_const', const=True,
+                    help="Log while running")
 
 args = parser.parse_args()
+
+if args.verbose is not None:
+    trace_level = args.verbose
 
 deadline = dateparser.parse(args.deadline)
 if deadline is None:
     print(f'{args.deadline} is not a correctly-formatted datetime.')
     exit(1)
 
-print(f'Deadline: {args.deadline} parsed as {deadline}')
+trace(f'Deadline: {args.deadline} parsed as {deadline}')
 
-check_recent_posts(mastodon, 'ben@social.python-gsoc.org', deadline)
+register_app()
+cred = cred_path.read_text()
 
+masto = Mastodon(
+    client_id,
+    cred,
+    api_base_url = python_gsoc_url
+)
+
+if args.account is not None:
+    # input validation?
+    check_recent_posts(masto, args.account, deadline)
+elif args.input is not None:
+    input_file = Path(args.input).resolve()
+    trace(f"Loading settings from {input_file}")
+    if not input_file.exists():
+        print(f"No such data source: '{input_file}'")
+        exit(1)
+
+    account_list_str = input_file.read_text()
+    account_list = account_list_str.splitlines()
+
+    for account in account_list:
+        trace(f'Calling check_recent_posts(...,{account},{deadline})')
+        check_recent_posts(masto, account, deadline)
+
+    print("Not ready for all the file decoding shenanigans yet!")
